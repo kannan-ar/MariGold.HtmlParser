944cbf3441a9756ee7c26a8d929504d79d0b349e
Kannan Ramachandran <kannanpala@gmail.com>
2015-10-15 07:26:58 +0530

    Implementing Attribute Selector

--- MariGold.HtmlParser/CSS/Selectors/AttributeSelector.cs
+++ MariGold.HtmlParser/CSS/Selectors/AttributeSelector.cs
@@ -8,8 +8,16 @@
     {
         private readonly Regex isValid;
         private readonly Regex spliter;
-        private readonly string currentSelector;
-        private readonly string selectorText;
+        private readonly AttributeElements element;
+
+        internal class AttributeElements
+        {
+            internal string SelectorText { get; set; }
+            internal string AttributeName { get; set; }
+            internal bool HasValue { get; set; }
+            internal Action Filter { get; set; }
+            internal string Value { get; set; }
+        }
 
         internal AttributeSelector(ISelectorContext context)
         {
@@ -20,16 +28,42 @@
 
             this.context = context;
 
-            isValid = new Regex("^\\[([a-zA-Z]+[0-9]*)+([~|\\^\\$\\*]?=+[\"']?([a-zA-Z]+[0-9]*)+[\"']?)*\\]$");
+            isValid = new Regex("^\\[([a-zA-Z]+[0-9]*)+([~|^$*]?=+[\"']?([a-zA-Z]+[0-9]*)+[\"']?)*\\]");
+            spliter = new Regex(@"\w+|[~|^$*]|=");
         }
 
-        internal AttributeSelector(string currentSelector, string selectorText, ISelectorContext context)
+        internal AttributeSelector(AttributeElements element, ISelectorContext context)
         {
-            this.currentSelector = currentSelector;
-            this.selectorText = selectorText;
+            this.element = element;
             this.context = context;
+        }
 
-            spliter = new Regex("([a-zA-Z]+[0-9]*)+|[~|\\^\\$\\*]?|=|");
+        private List<string> SplitSelector(string selector)
+        {
+            List<string> elements = new List<string>();
+
+            MatchCollection matches = spliter.Matches(selector);
+
+            foreach (Match match in matches)
+            {
+                if (!string.IsNullOrEmpty(match.Value))
+                {
+                    elements.Add(match.Value);
+                }
+            }
+
+            return elements;
+        }
+
+        private AttributeElements ParseSelector(string selector, Match match)
+        {
+            AttributeElements element = new AttributeElements();
+
+            string trimmedSelector = selector.Substring(match.Value.Length);
+
+            List<string> elements = SplitSelector(trimmedSelector);
+
+            return element;
         }
 
         internal override bool IsValidNode(HtmlNode node)
@@ -39,7 +73,16 @@
 
         internal override CSSelector Parse(string selector)
         {
-            throw new NotImplementedException();
+            Match match = isValid.Match(selector);
+
+            if (match.Success)
+            {
+                return new AttributeSelector(ParseSelector(selector, match), context);
+            }
+            else
+            {
+                return PassToSuccessor(selector);
+            }
         }
 
         internal override void Parse(HtmlNode node, List<HtmlStyle> htmlStyles)
