5dfba9984c4e4af231b7f6527eeb47e5caa6bf9a
Kannan Ramachandran <kannanpala@gmail.com>
2015-11-07 10:39:43 +0530

    Unit tests for external styles

---
+++ MariGold.HtmlParser.Tests/ExternalStyles.cs
@@ -0,0 +1,46 @@
+﻿namespace MariGold.HtmlParser.Tests
+{
+	using System;
+	using NUnit.Framework;
+	using MariGold.HtmlParser;
+	using System.Linq;
+	using System.IO;
+	
+	[TestFixture]
+	public class ExternalStyles
+	{
+		[Test]
+		public void BasicExternalStyleSheet()
+		{
+			string path = TestUtility.GetFolderPath("Html\\basicexternalstyle.htm");
+			string html = string.Empty;
+
+			using (StreamReader sr = new StreamReader(path))
+			{
+				html = sr.ReadToEnd();
+			}
+
+			HtmlParser parser = new HtmlTextParser(html);
+
+			Assert.AreEqual(true, parser.Parse());
+			parser.ParseCSS();
+			
+			Assert.IsNotNull(parser.Current);
+            
+			HtmlNode node = parser.Current.Children[0];
+			
+			while (node.Tag != "body")
+				node = node.Next;
+			
+			HtmlNode body = node;
+			
+			node = node.Children[0];
+			
+			while (node.Tag != "p")
+				node = node.Next;
+			
+			node.AnalyzeNode("p", "test", "<p>test</p>", body, false, true, 1, 0, 1);
+			node.Styles.CheckKeyValuePair(0, "font-size", "50px");
+		}
+	}
+}
